8e2250283ef35de781eae73f835eeede6aab1710
seghodavid <56871763+seghodavid@users.noreply.github.com>
2021-12-23 19:38:03 +0100

    Cipher update

--- Cipher.js
+++ Cipher.js
@@ -2,14 +2,16 @@ const crypto = require('crypto');
 const CryptoJS = require('crypto-js')
 
 const algorithm = 'aes-256-ctr';
-const secretKey = 'daviddivineseghosimheseghosimhed';
+const secretKey = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
 
 const encrypt = (text) => {
 
     const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
 
-    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    let encrypted =cipher.update(text)
+
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
 
     return {
         iv: iv.toString('hex'),
@@ -34,10 +36,10 @@ module.exports = {
 
 // Another way to decrypt and encrypt, you have to install and require crypto-js for this
 //encrypt
-// const ciphertext = CryptoJS.AES.encrypt("Hello World","david segho 123").toString();
+const ciphertext = CryptoJS.AES.encrypt("Hello World","david segho 123").toString();
 
-// //decrypt
-//  const bytes = CryptoJS.AES.decrypt(ciphertext,"david segho 123");
-//  const original = bytes.toString(CryptoJS.enc.Utf8);
+//decrypt
+ const bytes = CryptoJS.AES.decrypt(ciphertext,"david segho 123");
+ const original = bytes.toString(CryptoJS.enc.Utf8);
 
-//  console.log(original);
+ console.log(original);
